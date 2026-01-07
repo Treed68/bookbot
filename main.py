c23a5b2347b1b9124454f228926e46574ebf829e
Treed68 <treed68@hotmail.com>
2026-01-07 16:34:59 -0330

    bookbot CH3, L1 complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, get_sorted_chars
    
     
 
@@ -8,9 +8,17 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
+    #chars_dict = get_chars_dict(text)
+    chars_sort = get_sorted_chars(text)
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at", book_path)
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(chars_dict)
+    #print(chars_dict)
+    print("--------- Character Count -------")
+    for key, value in chars_sort.items():
+        print(f"{key}: {value}")
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -11,3 +11,30 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def get_sorted_chars(text):
+    chars_sort = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars_sort:
+            chars_sort[lowered] += 1
+        else:
+            chars_sort[lowered] = 1
+    
+    dict_sort ={key: value for key,
+                value in sorted(chars_sort.items(), 
+                                reverse=True,
+                                key=lambda item: item[1])
+                                if key.isalpha()}
+    
+        
+    
+    return dict_sort
+    
+
+    
+
+
+
+    
+
